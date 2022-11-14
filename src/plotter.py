6d33e5d138204e950f1616a167e41e54a535e505
Gaurang Tandon <1gaurangtandon@gmail.com>
2022-11-14 09:42:43 +0530

    Add basic compare

--- src/plotter.py
+++ src/plotter.py
@@ -7,6 +7,8 @@ import pandas as pd
 import requests
 
 from rich import print as rprint
+from rich.table import Table
+from rich.console import Console
 
 LIMIT_MAX = 100
 
@@ -60,50 +62,54 @@ def get_data_from_scoresaber(player_id: str, force_fetch: bool = False):
     json.dump(final_data, f)
   return final_data
 
-def get_all_scores(player_id: str, force_fetch: bool=False):
+def get_all_scores(player_id: str, force_fetch: bool=False, filter_fn=None):
   data = get_data_from_scoresaber(player_id, force_fetch)
   # certain songs have a zero maxScore, possibly they are unranked
   # so we filter them out
-  scores = list(filter(lambda x: x['leaderboard']['maxScore'], data['scores']))
+  scores = list(filter(filter_fn, filter(lambda x: x['leaderboard']['maxScore'], data['scores'])))
   return scores
-
+  
 def plot_time_chart(values, color='red'):
   plt.plot(values, color=color)
   plt.show()
 
-def get_raw_pp(scores, filter_fn=lambda x: True):
-  return list(map(lambda x: x['score']['pp'], filter(filter_fn, scores)))
+def get_raw_pp(scores):
+  return list(map(lambda x: x['score']['pp'], scores))
 
-def get_weighted_pp(scores, filter_fn=lambda x: True):
-  return list(map(lambda x: x['score']['pp'] * x['score']['weight'], filter(filter_fn, scores)))
+def get_weighted_pp(scores):
+  return list(map(lambda x: x['score']['pp'] * x['score']['weight'], scores))
 
-def get_stars(scores, filter_fn=lambda x: True):
-  return list(map(lambda x: x['leaderboard']['stars'], filter(filter_fn, scores)))
+def get_stars(scores):
+  return list(map(get_star_one_score, scores))
 
-def get_accuracy(scores, filter_fn=lambda x: True):
-  return list(map(lambda x: int(10000 * x['score']['modifiedScore'] / x['leaderboard']['maxScore']) / 100, filter(filter_fn, scores)))
+def get_star_one_score(score_obj):
+  return score_obj['leaderboard']['stars']
 
-def get_names(scores, filter_fn=lambda x: True):
-  return list(map(lambda x: x['leaderboard']['songName'] + ' - ' + x['leaderboard']['songAuthorName'], filter(filter_fn, scores)))
+def get_acc_one_score(score_obj):
+  return int(10000 * score_obj['score']['modifiedScore'] / score_obj['leaderboard']['maxScore']) / 100
+
+def get_accuracy(scores):
+  return list(map(get_acc_one_score, scores))
+
+def get_name_one_score(score_obj):
+  return score_obj['leaderboard']['songName'] + ' - ' + score_obj['leaderboard']['songAuthorName']
+
+def get_names(scores):
+  return list(map(get_name_one_score, scores))
 
 def plot_pp(scores):
   plot_time_chart(get_raw_pp(scores), color='blue')
   plot_time_chart(get_weighted_pp(scores), color='green')
 
 def plot_stars_matrix(scores):
-  # any song less than 5 star is not helping me get 200pp
-  # highest I have so far on a 4star song is 180pp on a 93% play
-  # so now I should switch to >= 5 star songs
-  # filter out low star songs
-  filter_fn = lambda x: x['leaderboard']['stars'] >= 5
-  stars = get_stars(scores, filter_fn)
-  accuracy = get_accuracy(scores, filter_fn)
-  names = get_names(scores, filter_fn)
-  raw_pp = get_raw_pp(scores, filter_fn)
-  highest = max(raw_pp)
-  lowest = min(raw_pp)
+  stars = get_stars(scores)
+  accuracy = get_accuracy(scores)
+  names = get_names(scores)
+  raw_pp = get_raw_pp(scores)
   highest_acc = max(accuracy)
   lowest_acc = min(accuracy)
+  # highest_pp = max(raw_pp)
+  # lowest_pp = min(raw_pp)
 
   # zip data
   data = list(zip(stars, accuracy, names, raw_pp))
@@ -113,7 +119,7 @@ def plot_stars_matrix(scores):
 
   # print for check
   def get_print_string(x):
-    # color = get_gradient(lowest, highest, x[3])
+    # color = get_gradient(lowest_pp, highest_pp, x[3])
     # formatted_pp = f"[{color}]{x[3]}[/]"
     color_acc = get_gradient(lowest_acc, highest_acc, x[1])
     formatted_accuracy = f"[{color_acc}]{x[1]}[/]"
@@ -129,6 +135,32 @@ def plot_stars_matrix(scores):
   # stars, accuracy, names, raw_pp = zip(*data)
   # plt.plot(stars, accuracy, color='red')
   # plt.show()
+
+def plot_comparison(scores, scores_player, name_player):
+  # tuples of (my score, their score, difference, song name)
+  diff_list = []
+  for score in scores:
+    songhash = score['leaderboard']['songHash']
+    comparable = None
+    for score_2 in scores_player:
+      if score_2['leaderboard']['songHash'] == songhash:
+        comparable = score_2
+        break
+    if comparable:
+      data = (get_acc_one_score(score), get_acc_one_score(comparable), get_star_one_score(score), get_name_one_score(score))
+      diff_list.append(data)
+  diff_list.sort(key=lambda x,y:x[0]-x[1]<y[0]-y[1])
+  table = Table(title="Score comparison")
+  table.add_column('My score')
+  table.add_column('Their score')
+  table.add_column('Difficulty')
+  table.add_column('Song name')
+  for row in diff_list:
+    table.add_row(*row)
+
+  console = Console()
+  console.print(table)
+
 
 if __name__ == "__main__":
   parser = ArgumentParser("ScoreSaber scorer")
@@ -150,6 +182,17 @@ if __name__ == "__main__":
     else:
       player_id = mapping[args.player]
 
-  scores = get_all_scores(player_id, args.force)
-  # plot_pp(scores)
-  plot_stars_matrix(scores)
+  # any song less than 5 star is not helping me get 200pp
+  # highest I have so far on a 4star song is 180pp on a 93% play
+  # so now I should switch to >= 5 star songs
+  # filter out low star songs
+  filter_fn = lambda x: x['leaderboard']['stars'] >= 5
+
+  scores = get_all_scores(player_id, args.force, filter_fn)
+  if other_player_id:
+    scores_other = get_all_scores(other_player_id, args.force, filter_fn)
+  
+  if args.compare:
+    plot_comparison(scores, scores_other, args.player)
+  else:
+    plot_stars_matrix(scores)
